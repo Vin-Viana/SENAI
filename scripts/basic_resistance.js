b3a60d014782faf946861eec311f4b1e0412b9d1
VV <vi.costasouza@hotmail.com>
2023-02-05 18:13:08 -0300

    Names and new commentaries

--- scripts/basic_resistance.js
+++ scripts/basic_resistance.js
@@ -1,17 +1,12 @@
 // Resistance Calculator
 
 /*
-
-Parallel and Series calculator
-
-*/
-
-var inputCount = 1;
-
+ * addInput() creates a new input box when the button is pressed
+ */
 function addInput() {
-  var inputsContainer = document.getElementById("inputsContainer");
-  var newInput = document.createElement("input");
-  newInput.type = "text";
+  var inputsContainer = document.getElementById("inputsEntries");
+  var newInput = document.createElement("inputAdditional");
+  newInput.type = "number";
   newInput.id = "input" + inputCount;
   inputsContainer.appendChild(newInput);
   inputCount++;
@@ -19,18 +14,17 @@ function addInput() {
 
 /*
 
+* We need to create a function that will calculate differently when more than 2 input boxes are being used.
+
+* The for loop below is being used to count through the array of new input boxes. It just don't work yet.
+
 function calculate(operation) {
   var result = parseFloat(document.getElementById("input1").value) || 0;
   for (var i = 1; i < inputCount; i++) {
     var value = parseFloat(document.getElementById("input" + i).value) || 0;
-    if (operation) {
-      if ("Series") result += value;
-    } else {
-      result * value;
-    }
   }
-  document.getElementById("result").innerHTML = result;
-}
+};
+
 */
 
 function calculate(operation) {
@@ -40,12 +34,12 @@ function calculate(operation) {
   switch (operation) {
     case "Series":
       let resultSum = input1 + input2;
-      document.getElementById("result").innerHTML = `${resultSum} Ohms`;
+      document.getElementById("result").innerHTML = `${resultSum} Ω`;
       break;
 
     case "Parallel":
       let resultParal = (input1 * input2) / (input1 + input2);
-      document.getElementById("result").innerHTML = `${resultParal} Ohms`;
+      document.getElementById("result").innerHTML = `${resultParal} Ω`;
       break;
 
     default:
