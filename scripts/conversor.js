be66840fcd4b22bc5976c00162a65cd24a79032b
Pedro_Dalapola <Pedrogomesdalapola007@gmail.com>
2023-02-05 20:11:26 -0300

    Add code and title

---
+++ scripts/conversor.js
@@ -0,0 +1,14 @@
+//Conversor: Octal to Decimal
+
+let octal = [37246];
+let dOctal = [...(octal + "")];
+let results2 = 0;
+dOctal.reverse();
+
+for (let [ind, value] of dOctal.entries()) {
+  let calcElevate = value * 8 ** ind;
+  results2 += calcElevate;
+  console.log(calcElevate);
+}
+
+console.log(results2);
