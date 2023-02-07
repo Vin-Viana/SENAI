feae6ac26a708d81888f51ea06f1d99dc8850f5f
Vinícius Viana <vi.costasouza@hotmail.com>
2023-02-07 01:36:08 -0300

    AddInput() fixed; New code (.reduce); Addition of a (switch-case); For-loop done; Added TBD additions.

--- scripts/basic_resistance.js
+++ scripts/basic_resistance.js
@@ -1,50 +1,95 @@
 // Resistance Calculator
 
-/*
- * addInput() creates a new input box when the button is pressed
- */
+const zeroValue = (document.getElementById("result").innerHTML = `0 Ω`);
 
-var inputCount = 1;
+var inputCount = 3;
+// addInput() creates a new input box when the button is pressed
 function addInput() {
   var inputsContainer = document.getElementById("inputsEntries");
-  var newInput = document.createElement("inputAdditional");
+  var newInput = document.createElement("input");
   newInput.type = "number";
-  newInput.id = "input" + inputCount;
+  newInput.id = "input-" + inputCount;
   inputsContainer.appendChild(newInput);
   inputCount++;
 }
 
-/*
-
-* We need to create a function that will calculate differently when more than 2 input boxes are being used.
-
-* The for loop below is being used to count through the array of new input boxes.
-* The calculation process is not working yet.
-
 function calculate(operation) {
-  for (var i = 1; i < inputCount; i++) {
-    var value = parseFloat(document.getElementById("input" + i).value) || 0;
-  }
-};
-
-*/
-
-function calculate(operation) {
+  // collect the freshier values from the input boxes
   var input1 = parseFloat(document.getElementById("input-1").value) || 0;
   var input2 = parseFloat(document.getElementById("input-2").value) || 0;
 
-  switch (operation) {
-    case "Series":
-      let resultSum = input1 + input2;
-      document.getElementById("result").innerHTML = `${resultSum} Ω`;
-      break;
+  if (document.getElementById("input-3")) {
+    // will be used later to get the lastest result without looping it
+    let resultArray = [];
 
-    case "Parallel":
-      let resultParal = (input1 * input2) / (input1 + input2);
-      document.getElementById("result").innerHTML = `${resultParal} Ω`;
-      break;
+    // will start from 3 because there's already 2 inputs (the default ones)
+    for (var i = 1; i < inputCount; i++) {
+      var value = parseFloat(document.getElementById("input-" + i).value) || 0;
 
-    default:
-      null;
+      switch (operation) {
+        case "Series":
+          // collects all the values from the extras input boxes
+          resultArray.push(value);
+
+          // '.reduce' is being used to accumulate each iteration of the array and suming it
+          // it keeps accumulating and doing the function until the last number on the array is reached
+          // when it's reached, the actualValue collects the last result of the function being done on 'return'
+          let resultSum = resultArray.reduce(function (
+            numArray,
+            actualValue = 0
+          ) {
+            // the actualValue needed to be defined first as 0, bc '.reduce' dont work with empty arrays
+            return numArray + actualValue;
+          });
+          // now it finally collected the last value from the array after looping all and replaced result id
+          document.getElementById("result").innerHTML = `${resultSum} Ω`;
+          break;
+
+        case "Parallel":
+          //prettier-ignore
+          let resultParal = parseFloat(1/((1/input1) + (1/input2) + (1/value)));
+          resultArray.push(resultParal);
+
+          // now it finally collected the last value from the array after looping all and replaced result id
+          document.getElementById("result").innerHTML = `${
+            resultArray[resultArray.length - 1]
+          } Ω`;
+          break;
+
+        default:
+          zeroValue;
+          break;
+      }
+    }
+  } else {
+    switch (operation) {
+      case "Series":
+        let resultSum = input1 + input2;
+        document.getElementById("result").innerHTML = `${resultSum} Ω`;
+        break;
+
+      case "Parallel":
+        let resultParal = (input1 * input2) / (input1 + input2);
+        document.getElementById("result").innerHTML = `${resultParal} Ω`;
+        break;
+
+      default:
+        zeroValue;
+        break;
+    }
   }
 }
+
+/*
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * * * Future ideas  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * 1. Put the basic code (2 inputs) inside a modal box on the index page.
+ * 2. Insert a button to a 'more complex calculation' inside this modal, and it will switch to the new page containing all this code
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * 1. Create a history of already calculated numbers
+ * 2. Add these values inside an accordion
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * * * END.  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ */
