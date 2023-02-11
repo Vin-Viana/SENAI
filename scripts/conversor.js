5cbf71b5cb7613e87147f436cd5bf74f9ca3f7ac
Pedro_Dalapola <Pedrogomesdalapola007@gmail.com>
2023-02-11 17:22:12 -0300

    Put the link, the HTML and the JS of the conversor

--- scripts/conversor.js
+++ scripts/conversor.js
@@ -1,15 +1,42 @@
 //Conversor: Octal to Decimal
 
-let octal = [37246];
-//To separate the numbers
-let dOctal = [...(octal + "")];
-let results2 = 0;
-dOctal.reverse();
+const zeroValue = (document.getElementById("result").innerHTML = `0`);
 
-for (let [ind, value] of dOctal.entries()) {
-  let calcElevate = value * 8 ** ind;
-  results2 += calcElevate;
-  console.log(calcElevate);
+function calculate(operation) {
+  var input1 = parseFloat(document.getElementById("input-1").value) || 0;
+  let resultArray = [];
+
+  switch (operation) {
+    case "Binary":
+      //Still in progress
+      break;
+
+    case "Octa":
+      //Still in progress
+      break;
+
+    case "Decimal":
+      let octal = parseFloat(input1);
+      let dOctal = [...(octal + "")];
+      let results2 = 0;
+      dOctal.reverse();
+
+      for (let [ind, value] of dOctal.entries()) {
+        let calcElevate = value * 8 ** ind;
+        results2 += calcElevate;
+        resultArray.push(results2);
+        document.getElementById("result").innerHTML = `${
+          resultArray[resultArray.length - 1]
+        } Decimal`;
+      }
+      break;
+
+    case "Hexadecimal":
+      //Still in progress
+      break;
+
+    default:
+      zeroValue;
+      break;
+  }
 }
-
-console.log(results2);
